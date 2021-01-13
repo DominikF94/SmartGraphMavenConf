3cde7ca904434b0ceaf4360d7bd27549bfbc00c1
Dominik <dominik.flohs@googlemail.com>
2021-01-13 23:02:28 +0100

    added moule-info to the project and so that the imported library is later modular as well. Exports all its packages to importing libraries.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module SmartGraph{
+    requires javafx.controls;
+    requires java.logging;
+
+    exports com.brunomnsilva.smartgraph.graphview;
+    exports com.brunomnsilva.smartgraph.graph;
+    exports com.brunomnsilva.smartgraph.tree;
+    exports com.brunomnsilva.smartgraph.containers;
+    exports com.brunomnsilva.smartgraph;
+}
